abeb82c836dcd624b4163e59ffd31228b163a3b0
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2023-08-24 16:30:58 -0500

    connected frontend website to firebase backend

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDIvjMfNmUtDkaQmEiEu3D0ib1RhMI-o8Q",
+  authDomain: "mso-violin-i-rotation.firebaseapp.com",
+  projectId: "mso-violin-i-rotation",
+  storageBucket: "mso-violin-i-rotation.appspot.com",
+  messagingSenderId: "1032349611479",
+  appId: "1:1032349611479:web:5d03da1703b054e43c8792",
+};
+
+initializeApp(firebaseConfig);
