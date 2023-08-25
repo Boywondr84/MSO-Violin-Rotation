c3e44079cda2b47acb39227347b39f622ddec3ce
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2023-08-24 19:02:51 -0500

    beginning work on input form fields

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-import { initializeApp } from "firebase/app";
-
+// CSS imports
 import "spectre.css";
 import "./style.css";
+
+// Firebase import
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDIvjMfNmUtDkaQmEiEu3D0ib1RhMI-o8Q",
@@ -13,3 +16,38 @@ const firebaseConfig = {
 };
 
 initializeApp(firebaseConfig);
+
+const DB = getFirestore();
+
+// collection reference from DB
+const COLREF = collection(DB, "Violinists");
+
+// Violinists DB Data
+let violinistsData = [{}];
+
+// get by ID form
+const GET_MSO_VIOLINIST = document.getElementById("get-violinist-select");
+const GET_MSO_DATA = document.getElementsByClassName("getID");
+
+// get all collection data
+getDocs(COLREF).then((snapshot) => {
+  snapshot.docs.forEach((doc) => {
+    violinistsData.push({ ...doc.data(), id: doc.id });
+  });
+  for (let i = 1; i <= violinistsData.length; i++) {
+    console.log(violinistsData[1]);
+
+    // Violinists sorted by position
+    const SORTVIOLINS = violinistsData;
+    SORTVIOLINS.sort((a, b) => {
+      return a.position - b.position;
+    });
+
+    // get form pull names from DB and uses ID values to submit for data retrieval
+    // get by id form action begins at line
+    let getSelectedViolinist = document.createElement("option");
+    getSelectedViolinist.text = `${violinistsData[i].firstName} ${violinistsData[i].lastName}`;
+    getSelectedViolinist.value = violinistsData[i].id;
+    GET_MSO_VIOLINIST.appendChild(getSelectedViolinist);
+  }
+});
